bfe3ff7a38055cf8a4b0cd2a5338aafaf2ff2a0b
hhatuey <hatmiju@gmail.com>
2024-03-16 14:16:06 -0500

    feat(ui): created UI Header, added in core app

--- apps/core/src/layout/index.tsx
+++ apps/core/src/layout/index.tsx
@@ -1,13 +1,16 @@
 import { React, ReactSubApp, createDynamicComponent, staticPropsFeature } from "@xarc/react";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Header } from "@teleoffice/ui";
+import "bootstrap/dist/css/bootstrap.min.css";
 import electrodePng from "../../static/electrode.png";
 import { message } from "./message";
 
 const Layout = (props) => {
-  import('bootstrap/dist/js/bootstrap.bundle');
+  import("bootstrap/dist/js/bootstrap.bundle");
   return (
     <>
-      <header className="p-3">Header Component here</header>
+      <header className="p-3">
+        <Header />
+      </header>
       <main>
         <aside>SideNav Component here</aside>
         <section>Container Component here</section>
