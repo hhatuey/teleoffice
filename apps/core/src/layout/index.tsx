6f16a6946f5fe9d48551bf6412e885056398df92
hhatuey <hatmiju@gmail.com>
2024-03-16 21:34:52 -0500

    feat(core): enabled client side routing

--- apps/core/src/layout/index.tsx
+++ apps/core/src/layout/index.tsx
@@ -1,11 +1,12 @@
-import { React, ReactSubApp, createDynamicComponent, staticPropsFeature } from "@xarc/react";
+import { React, ReactSubApp } from "@xarc/react";
+import { reactRouterFeature, Route, Routes } from "@xarc/react-router";
 import { Header, Footer, SideNav } from "@teleoffice/ui";
 import "bootstrap/dist/css/bootstrap.min.css";
 import electrodePng from "../../static/electrode.png";
 import { copyRightMessage } from "../info";
 
 const Layout = (props) => {
-  import('bootstrap/dist/js/bootstrap.bundle');
+  import("bootstrap/dist/js/bootstrap.bundle");
   return (
     <>
       <header className="p-3">
@@ -15,7 +16,15 @@ const Layout = (props) => {
         <aside className="col-auto">
           <SideNav />
         </aside>
-        <section className="col">Container Component here</section>
+        <section className="col">
+          <Routes>
+            <Route path="/" element={<div>Home subapp</div>} />
+            <Route path="/dashboard" element={<div>Dashboard subapp</div>} />
+            <Route path="/orders" element={<div>Orders subapp</div>} />
+            <Route path="/products" element={<div>Products subapp</div>} />
+            <Route path="/customers" element={<div>Customers subapp</div>} />
+          </Routes>
+        </section>
       </main>
       <footer className="p-3">
         <Footer logoApp={electrodePng} legalMsg={copyRightMessage} />
@@ -26,4 +35,5 @@ const Layout = (props) => {
 
 export const subapp: ReactSubApp = {
   Component: Layout,
+  wantFeatures: [reactRouterFeature({ React, history: true })],
 };
